ec3c990ed89da73202e12927517454ce77c5c7cf
bhutuklearning <amritanshugoutam.dev@gmail.com>
2025-09-01 00:06:15 +0530

    The foundatioin model of the blog app is initiated.

--- backend/src/models/blog.model.js
+++ backend/src/models/blog.model.js
@@ -1,6 +1,73 @@
 import mongoose from "mongoose";
 
-const blogSchema = new mongoose.Schema({}, { timestamps: true });
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+        trim: true,
+        maxlength: [150, "Title cannot exceed 150 characters"]
+    },
+    content: {
+        type: String,
+        required: [true, "Content is required"],
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // connects with User.blogs virtual
+        required: true,
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User", // connects with User.likedBlogs virtual
+        },
+    ],
+    dislikes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    comments: [
+        {
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User", // will populate user name/email in comments
+                required: true,
+            },
+            text: {
+                type: String,
+                required: true,
+                trim: true,
+                maxlength: 300,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now().toLocaleString(),
+            },
+        },
+    ],
+}, {
+    timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true },
+});
+
+//  Virtuals for counts
+blogSchema.virtual("likeCount").get(function () {
+    return this.likes?.length || 0;
+});
+
+blogSchema.virtual("dislikeCount").get(function () {
+    return this.dislikes?.length || 0;
+});
+
+blogSchema.virtual("commentCount").get(function () {
+    return this.comments?.length || 0;
+});
+
+// Full-text index for searching blogs
+blogSchema.index({ title: "text", content: "text" });
+
+
 
 const Blog = mongoose.model("Blog", blogSchema);
 export default Blog;
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -3,30 +3,107 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, "Name is required"],
         trim: true,
-        maxlength: 50
+        maxlength: [50, "Name cannot exceed 50 characters"]
     },
     email: {
         type: String,
-        required: true,
+        required: [true, "Email is required"],
         unique: true,
         trim: true,
+        lowercase: true,
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Please enter a valid email address"],
     },
     password: {
         type: String,
-        required: true,
-        minlength: 8
+        required: [true, "Password is required"],
+        minlength: [8, "Password must be at least 8 characters long"],
+        select: false // Hide password by default when fetching user
     },
-
-
+    bio: {
+        type: String,
+        trim: true,
+        maxlength: 200
+    },
+    socials: {
+        linkedin: {
+            type: String,
+            match: [
+                /^https?:\/\/(www\.)?linkedin\.com\/.+$/,
+                "Invalid LinkedIn profile URL",
+            ],
+        },
+        instagram: {
+            type: String,
+            match: [
+                /^https?:\/\/(www\.)?instagram\.com\/[A-Za-z0-9_.]+$/,
+                "Invalid Instagram profile URL",
+            ],
+        },
+        x: {
+            type: String,
+            match: [
+                /^https?:\/\/(twitter\.com|x\.com)\/[A-Za-z0-9_]+$/,
+                "Invalid X/Twitter profile URL",
+            ],
+        },
+    },
+    lastLogin: {
+        type: Date,
+        default: null
+    }
 },
     {
-        timestamps: true
-    });
+        timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true }
+    }
+);
+
+
+// Virtual: Blogs authored by the user
+userSchema.virtual("blogs", {
+    ref: "Blog",
+    localField: "_id",
+    foreignField: "author",
+});
+
+// Virtual: Blog count
+userSchema.virtual("blogCount", {
+    ref: "Blog",
+    localField: "_id",
+    foreignField: "author",
+    count: true,
+});
+
+// Virtual: Number of blogs the user has liked
+userSchema.virtual("likedBlogs", {
+    ref: "Blog",
+    localField: "_id",
+    foreignField: "likes", // because Blog.likes stores userIds
+    count: true,
+});
+
+// Virtual: Total likes received on the user’s blogs
+userSchema.virtual("totalLikesReceived", {
+    ref: "Blog",
+    localField: "_id",
+    foreignField: "author",
+    justOne: false,
+    options: {}, // placeholder (we’ll aggregate in controller)
+});
+
+// Virtual: Total comments received on the user’s blogs
+userSchema.virtual("totalCommentsReceived", {
+    ref: "Blog",
+    localField: "_id",
+    foreignField: "author",
+    justOne: false,
+    options: {}, // placeholder (we’ll aggregate in controller)
+});
+
 
 // Create text index for search functionality
-userSchema.index({ name: 'text', email: 'text' });
+userSchema.index({ name: 'text', email: 'text', bio: "text" });
 
 const User = mongoose.model('User', userSchema);
 export default User;
